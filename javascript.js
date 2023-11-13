8a00b46d45665c71c5e695a11b15dd2ae4cf8961
Ismail <141173099+alcode111@users.noreply.github.com>
2023-11-13 11:19:21 +0100

    Create score variables and select relevant elements

---
+++ javascript.js
@@ -0,0 +1,15 @@
+let homeScore = 0;
+let guestScore = 0;
+
+let homeScoreDisplay = document.getElementById("home-score");
+let guestScoreDisplay = document.getElementById("guest-score");
+
+let homePlusOneBtn = document.getElementById("home-plus-one");
+let homePlusTwoBtn = document.getElementById("home-plus-two");
+let homePlusThreeBtn = document.getElementById("home-plus-three");
+
+let guestPlusOneBtn = document.getElementById("guest-plus-one");
+let guestPlusTwoBtn = document.getElementById("guest-plus-two");
+let guestPlusThreeBtn = document.getElementById("guest-plus-three");
+
+let btnRestart = document.getElementById("btn-restart")
